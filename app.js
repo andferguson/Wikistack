227bbf48804f6fdae6830872e60500e5502a1968
Andrew Ferguson <andrewmf@umich.edu>
2018-09-21 11:14:59 -0500

    syncing with db in app.js

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const main = require('./views/main');
+const morgan = require("morgan");
+const main = require("./views/main");
+const { db, User, Page} = require("./models");
 
-app.use(morgan('dev'));
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+app.use(morgan("dev"));
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    res.send(main(''));
-})
+app.get("/", (req, res) => {
+  res.send(main(""));
+});
 
-const PORT = 3000;
-app.listen(PORT, () => {
+const init = async () => {
+// this drops all tables then recreates them based on our JS definitions
+// models.db.sync({force: true})
+  await User.sync();
+  await Page.sync();
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`);
   });
+};
+
+init();
